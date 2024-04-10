e56209c05afc12b2bc593d0b38f38a6d805816d9
wrfmartinez <wrmartinez709@gmail.com>
2024-04-10 15:19:52 -0400

    Implement part of runGame function and fade out animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+const body = document.querySelector("body");
+const startButton = document.getElementById("start-btn");
+
+const runGame = () => {
+  const gameIntro = document.getElementById("game-intro");
+  gameIntro.classList.add("fade-out");
+  startButton.classList.add("fade-out");
+  setTimeout(() => {
+    gameIntro.classList.add("remove");
+    startButton.classList.add("remove");
+  }, 1200)
+}
+
+startButton.addEventListener("click", runGame);
